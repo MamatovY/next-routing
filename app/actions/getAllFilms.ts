b13fde0b85425b30b6bcee2fa0a07f731cb335d7
MamatovY <mamatovyiman28@gmail.com>
2023-09-11 22:42:16 +0600

    add typescript

--- app/actions/getAllFilms.ts
+++ app/actions/getAllFilms.ts
@@ -1,7 +1,6 @@
-export function getAllFilms() {
+export function getAllFilms(): Promise<FilmsResponse> {
     //cache: 'no-store' //SSR - getServerSideProps
     //cache: 'force-cache' //SSG - getStaticProps
     //next:{ revalidate:60 } // ISR - getStaticProps and revalidate 60s
-    return fetch('https://swapi.tech/api/films')
-        .then(res => res.json())
+    return fetch('https://swapi.tech/api/films').then(res => res.json())
 }
